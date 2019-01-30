46263ffa19056c9641da043288acc49a2bbf77d5
sandhyasrini <sandy150225@gmail.com>
2019-01-30 14:46:46 +0530

    Update product.test.js

--- test/product.test.js
+++ test/product.test.js
@@ -9,7 +9,7 @@ chai.use(chaiHttp);
 describe('/GET product', function() {
   it('should calculate the products of two arguments', function(done) {
     chai.request(server)
-      .get('/product?number1=5&number2=10') 
+      .get('/product/5/10') 
       .end((err, res) => {
         expect(res).to.have.status(200);
         res.text.should.equal('The product of 5 * 10 is 50');
@@ -19,7 +19,7 @@ describe('/GET product', function() {
 
   it('should not calculate the products of two arguments if the query parameter is not a number', function(done) {
     chai.request(server)
-      .get('/product?number1=a&number2=10') 
+      .get('/product/a/10') 
       .end((err, res) => {
         expect(res).to.have.status(400);
         res.text.should.equal('Not a number');
