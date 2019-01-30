4282866336e695d827c86113510fcc866b442329
sandhyasrini <sandy150225@gmail.com>
2019-01-30 14:27:24 +0530

    error handling

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,11 +1,11 @@
 const product = require("express")();
 
-product.get('/product', (req, res) => {
+product.get('/product/:number1/:number2', (req, res) => {
 
-    //Checking if the value passed in query string is a number
-    if (!isNaN(req.query.number1) && !isNaN(req.query.number2)) {
-        let result = req.query.number1 * req.query.number2;
-        res.send("The product of " + req.query.number1 + " * " + req.query.number2 + " is " + result)
+    //Checking if the value passed in params string is a number
+    if (!isNaN(req.params.number1) && !isNaN(req.params.number2)) {
+        let result = req.params.number1 * req.params.number2;
+        res.send("The product of " + req.params.number1 + " * " + req.params.number2 + " is " + result)
     } else {
         res.status(400).send("Not a number")
     }
